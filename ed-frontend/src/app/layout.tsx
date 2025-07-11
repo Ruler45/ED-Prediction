3c3f94481bbed964da0de3333207ea6286863bf6
Sahin <sahin0945@gmail.com>
2025-07-11 10:43:26 +0530

    Integrated API

--- ed-frontend/src/app/layout.tsx
+++ ed-frontend/src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Next.js 15 | Sahin Alam",
+  title: "Electrodeposition Predictor | Sahin Alam",
   description: "Created by Sahin Alam",
 };
 
--- ed-frontend/src/app/page.tsx
+++ ed-frontend/src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { ModeToggle } from "@/components/ui/modeToggle";
 import { Label } from "@radix-ui/react-label";
 import { useState } from "react";
